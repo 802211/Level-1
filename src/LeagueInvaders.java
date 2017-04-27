bb5c7b4b17a97257078d2c72733a7e54ff2f3fc8
XI MingPing <league@WTS-iMac-11.attlocal.net>
2017-04-26 18:36:39 -0700

    LeagueInvaders GameObject Game Panel and Projectiles
    Pt 6 - step 10

---
+++ src/LeagueInvaders.java
@@ -0,0 +1,31 @@
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+
+	LeagueInvaders(){
+		f.setSize(WIDTH, HEIGHT);
+		gp = new GamePanel();
+		setup();
+	}
+	
+	public static void main(String[] args) {
+		LeagueInvaders LI = new LeagueInvaders();
+		//LI.setup();
+	}
+	
+	JFrame f = new JFrame();
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel gp;
+	
+	void setup() {
+		f.add(gp);
+	
+		f.setVisible(true);
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+gp.startGame();
+	f.addKeyListener(gp);
+	}
+	
+}
